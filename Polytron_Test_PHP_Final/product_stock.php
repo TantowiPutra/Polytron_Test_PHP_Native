e6aa5bcb75932568af33d70fc9be0226f4b276f0
Tantowi Putra Agung Setiawan <tantowi6758@gmail.com>
2023-03-13 22:18:27 +0700

    Finished search feature for product_stock

--- Polytron_Test_PHP_Final/product_stock.php
+++ Polytron_Test_PHP_Final/product_stock.php
@@ -9,6 +9,17 @@ $password = $_SESSION['password'];
 
 require_once 'koneksi.php';
 
+// Tangkap Data Item Stock
+$sql = "SELECT *
+        FROM item_stocks ist
+                JOIN 
+                    locations l 
+                    ON ist.FK_locationcode = l.id
+                JOIN 
+                    items i 
+                    ON ist.FK_itemcode = i.id
+    ";
+
 // Menampilkan total stok untuk tiap barang untuk tiap lokasi
 $sql2 = "SELECT FK_locationcode,
                     FK_itemcode,
@@ -21,32 +32,47 @@ $sql2 = "SELECT FK_locationcode,
                     locations l
                     ON ist.FK_locationcode = l.id
                 JOIN items i
-                    ON ist.FK_itemcode = i.id
-            GROUP BY FK_locationcode, FK_itemcode
-            ORDER BY FK_locationcode
+                    ON ist.FK_itemcode = i.id 
     ";
 
-// Tangkap Data Item Stock
-$sql = "SELECT *
-        FROM item_stocks ist
-                JOIN 
-                    locations l 
-                    ON ist.FK_locationcode = l.id
-                JOIN 
-                    items i 
-                    ON ist.FK_itemcode = i.id
-    ";
-
+$isAlready = false;
 // Query untuk search
 if (isset($_POST['search_location'])) {
     if ($_POST['search_location'] != null) {
         $search = $_POST['search_location'];
 
-        $sql = $sql . "WHERE location_code = '$search' AND balance > '0'";
+        $sql = $sql . "WHERE location_code = '$search'";
+        $sql2 = $sql2 . "WHERE location_code = '$search'";
+    }
+
+    if (isset($_POST['search_item_code'])) {
+        if ($_POST['search_item_code'] != null) {
+            $search = $_POST['search_item_code'];
+
+            $sql = $sql . "AND item_code = '$search'";
+            $sql2 = $sql2 . "AND item_code = '$search'";
+            $isAlready = true;
+        }
     }
 }
 
+if ($isAlready == false) {
+    if (isset($_POST['search_item_code'])) {
+        if ($_POST['search_item_code'] != null) {
+            $search = $_POST['search_item_code'];
+
+            $sql = $sql . "WHERE item_code = '$search'";
+            $sql2 = $sql2 . "WHERE item_code = '$search'";
+        }
+    }
+}
+
+if (isset($_POST['search_item_code']) || isset($_POST['search_location'])) {
+    $sql = $sql . "AND balance > '0'";
+}
+
 $sql = $sql . "ORDER BY i.item_name, ist.date_input ASC";
+$sql2 = $sql2 . "GROUP BY FK_locationcode, FK_itemcode ORDER BY FK_locationcode";
 
 // Query Product_Stock
 $result = mysqli_query($connect, $sql);
@@ -58,11 +84,11 @@ if (!$result) {
     echo "Query Gagal 1";
 }
 
-$sql_search = "SELECT * FROM locations";
-$search_result = mysqli_query($connect, $sql_search);
-if (!$result) {
-    echo "Query Gagal 2";
-}
+$sql_location = "SELECT * FROM locations";
+$search_result_location = mysqli_query($connect, $sql_location);
+
+$sql_item_code = "SELECT * FROM items";
+$search_result_code = mysqli_query($connect, $sql_item_code);
 
 ?>
 
@@ -90,17 +116,33 @@ if (!$result) {
     <hr style="margin-bottom: 30px; margin-top: 30px;">
     <div style="text-align: center;">
         <form action="product_stock.php" method="POST">
-            <select name="search_location" id="search_location">
-                <option value="">Search All</option>
-                <?php
-                while ($data = mysqli_fetch_array($search_result)) {
-                ?>
-                    <option value="<?php echo $data['location_code'] ?>"><?php echo $data['location_code'] ?></option>
-                <?php
-                }
-                ?>
-            </select>
-            <button type="submit">CARI LOKASI</button>
+            <div class="mt-3">
+                <label for="search_location">Cari Lokasi: </label>
+                <select name="search_location" id="search_location">
+                    <option value="">Search All</option>
+                    <?php
+                    while ($data = mysqli_fetch_array($search_result_location)) {
+                    ?>
+                        <option value="<?php echo $data['location_code'] ?>"><?php echo $data['location_code'] ?></option>
+                    <?php
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="mt-3">
+                <label for="search_location">Cari Item Code: </label>
+                <select name="search_item_code" id="search_item_coded">
+                    <option value="">Search All</option>
+                    <?php
+                    while ($data = mysqli_fetch_array($search_result_code)) {
+                    ?>
+                        <option value="<?php echo $data['item_code'] ?>"><?php echo $data['item_code'] ?></option>
+                    <?php
+                    }
+                    ?>
+                </select>
+            </div>
+            <button type="submit" class="mt-3">SUBMIT</button>
         </form>
     </div>
 
